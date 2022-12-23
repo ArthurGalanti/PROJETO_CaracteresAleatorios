9bdadd85198452329b99a8822157e8f8e84dfe11
ArthurGalanti <arthur.galanti@hotmail.com>
2022-12-23 06:02:59 -0300

    Adicionado loop final

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿var quantidadeNomes = 99;
+var quantidadeLetras = 16;
+string nome;
+var listaCaracteres = new char[] { ' ', ' ', ' ' };
+var rand = new Random();
+
+for (int i = 0; i < quantidadeNomes; i++)
+{
+    nome = $"{(i + 1).ToString("00")}.|";
+    for (int j = 0; j < quantidadeLetras; j++)
+    {
+        nome += listaCaracteres[rand.Next(0, listaCaracteres.Length)];
+
+    }
+    nome += "|";
+    Console.WriteLine(nome);
+}
